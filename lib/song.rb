977a3203e5f423729023a12028a0044302188ef1
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-04 11:51:12 -0500

    commit tree e008394f07c3d9639f77ed15b12c86b4be4e4e86

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,13 @@ class Song
     @album = album
   end
 
+  def self.all
+    sql = "SELECT * FROM SONGS"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.drop_table
     sql = <<-SQL
       DROP TABLE IF EXISTS songs
@@ -26,6 +33,17 @@ class Song
     SQL
 
     DB[:conn].execute(sql)
+  end
+
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM songs WHERE name = ? LIMIT 1"
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
